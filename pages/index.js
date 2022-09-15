5935d184b57ba9b4c6404c8da5de3f93257d19f9
ruba <rubag@LAPTOP-OKS2LNHF>
2022-09-15 20:40:29 +0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          ruba <a href="https://nextjs.org"> alghamdi !</a>
         </h1>
 
         <p className={styles.description}>
